072aca07be5fbcb9261b9b2bcfb65153546f7139
Nika <nikaonime@gmail.com>
2023-07-08 13:54:37 +0300

    uuid, addTodo, useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 import AddTodo from './components/AddTodo/AddTodo';
 import Todolist from './components/TodoList/Todolist';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, title: 'to buy a shampoo' },
     { id: 2, title: 'to buy a T-shirt' },
-  ]
+  ])
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: todo
+    }
+    setTodos([newTodo, ...todos])
+  }
 
   return (
     <div className="App">
-      <AddTodo />
+      <AddTodo addTodo={addTodo}/>
       <Todolist todos={todos} />
     </div>
   );
--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AddTodo = () => {
+const AddTodo = ({ addTodo }) => {
+
+    const [value, setValue] = useState('')
+
+    const onChangeValue = (e) => {
+        setValue(e.target.value)
+    }
+
     return (
-        <div>AddTodo</div>
+        <div>
+            <input
+                type='text'
+                placeholder='Enter task'
+                value={value}
+                onChange={onChangeValue}
+            />
+            <button onClick={() => addTodo(value)}>add</button>
+        </div>
     )
 }
 
